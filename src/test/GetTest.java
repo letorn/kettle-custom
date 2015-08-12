88dabd342302b3bd6072f564f6071bb60276285d
Letorn <letorn@163.com>
2015-08-12 18:18:46 +0800

    update

--- src/test/GetTest.java
+++ src/test/GetTest.java
@@ -17,7 +17,7 @@ public class GetTest {
 
 		String appName = "职场导航网络科技有限公司";
 		String taskOId = "hbjyweb_webservice_cb20_xml_task";
-		String params = "<maps><map><key><![CDATA[AAB003]]></key><value>07957472-7</value></map><map><key><![CDATA[AAB004]]></key><value>职场导航</value></map><map><key><![CDATA[AAF036_1]]></key><value>19800101</value></map><map><key><![CDATA[AAF036_2]]></key><value>20151231</value></map><map><key><![CDATA[AAE022]]></key><value>%</value></map></maps>";
+		String params = "<maps><map><key><![CDATA[AAB004]]></key><value><![CDATA[%]]></value></map><map><key><![CDATA[AAB003]]></key><value><![CDATA[%]]></value></map><map><key><![CDATA[AAE022]]></key><value><![CDATA[%]]></value></map><map><key><![CDATA[AAF036_1]]></key><value><![CDATA[20140101]]></value></map><map><key><![CDATA[AAF036_2]]></key><value><![CDATA[20140201]]></value></map></maps>";
 
 		Get dxGet = new Get(url, username, password);
 		String[] strs = dxGet.invoke(appName, taskOId, params);
